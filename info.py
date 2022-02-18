b1153f86d189d9409bda2b0140d7d15f60864c12
Your Name <you@example.com>
2022-02-18 17:27:46 +0800

    final pt. 2

---
+++ info.py
@@ -0,0 +1,4 @@
+file = open('info.txt', 'w')
+
+file.write("Name: Irish Del Rosario\nAge: 19\nLocation: Rizal, Philippines\nEmail: aaskeladd@gmail.com\n \nDate: 02-18-22\nTime: 16:51:52")
+file.close() 
