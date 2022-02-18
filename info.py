70ae687aca174c99a3aa9066b40d81ef669de41a
Irish Khristine M. Del Rosario <92616151+irishhhhh@users.noreply.github.com>
2022-02-18 17:28:54 +0800

    Delete info.py

--- info.py
+++
@@ -1,4 +0,0 @@
-file = open('info.txt', 'w')
-
-file.write("Name: Irish Del Rosario\nAge: 19\nLocation: Rizal, Philippines\nEmail: aaskeladd@gmail.com\n \nDate: 02-18-22\nTime: 16:51:52")
-file.close() 
